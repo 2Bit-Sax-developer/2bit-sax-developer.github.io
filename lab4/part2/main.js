a39c116a2e323f8c0abe1085be476a3dd50f73ed
Thomas McLean <tmclean1403@gmail.com>
2025-08-01 13:25:01 -0400

    Declared imageFIles Array and imageAlts object
    Declared an array and object to hold the images and their alt texts as instructed in the lab

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -13,8 +13,22 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFiles = [
+    "pic1.jpg",
+    "pic2.jpg",
+    "pic3.jpg",
+    "pic4.jpg",
+    "pic5.jpg"
+];
 
 /* Declaring the alternative text for each image file */
+const imageAlts = {
+    "pic1.jpg" : "A close up picture of a blue eye on a white person with blonde eyebrows",
+    "pic2.jpg" : "A picture of an odd, wavy rock formation caused by erosion",
+    "pic3.jpg" : "A picture of purple and white flowers blooming",
+    "pic4.jpg" : "A picture of an ancient Egyptian mural, depicting the gods Iris, wearing rainbow wings, Anubis, having the head of the Jackal, and Ra, wearing clothing of a Pharoah",
+    "pic5.jpg" : "A picture of a beautiful brown butterfly resting on a large green leaf with its wings spread"
+}
 
 /* Looping through images */
 
