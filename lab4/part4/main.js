8ef179d3226c7ba453b33cdd8338fd3c0878af63
Thomas McLean <tmclean1403@gmail.com>
2025-08-01 14:23:58 -0400

    Added draw() method to EvilCircle Class

--- lab4/part4/main.js
+++ lab4/part4/main.js
@@ -124,6 +124,13 @@ class EvilCircle extends Shape {
         });
     }
 
+    draw() {
+        ctx.beginPath();
+        ctx.strokeStyle = this.color;
+        ctx.lineWidth = 3;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
 }
 
 //array to store the balls
